582dd3e8344de73f7c33167ac7b9f1f82e09a854
WhiteQube <vaisakhkmohan@gmail.com>
2021-03-26 00:16:00 -0700

    Update Api.kt

--- app/src/main/java/com/vaisakh/hustler/retrofit_recyclerview_kotlin_mvvm/Api.kt
+++ app/src/main/java/com/vaisakh/hustler/retrofit_recyclerview_kotlin_mvvm/Api.kt
@@ -9,15 +9,21 @@ import retrofit2.Response
 
 
 class Api(context2: Context){
-
-    var context : Context =context2
-
+    lateinit var context : Context
+    lateinit var  destinationService : DataService
+    init {
+         context  = context2
+         destinationService = ServiceBuilder.buildService(DataService::class.java)
+    }
     interface FetchListener {
             fun onFetched(result: Response<List<DataModel>>, error: String?)
     }
 
+    interface Fetchmodeldata {
+        fun onmodeldata(result: Response<DataModel>, error: String?)
+    }
+
     fun loadDestinations(listener: FetchListener) {
-        val destinationService = ServiceBuilder.buildService(DataService::class.java)
         val filter = HashMap<String, String>()
         // filter["country"] = "India"
         val requestCall = destinationService.getDatalist(filter)
@@ -34,6 +40,83 @@ class Api(context2: Context){
             }
             override fun onFailure(call: Call<List<DataModel>>, t: Throwable) {
                 Toast.makeText(context, "Error Occurred" + t.toString(), Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    fun loadSingleData(id: Int,listener: Fetchmodeldata) {
+        val requestCall = destinationService.getDestination(id)
+        requestCall.enqueue(object : retrofit2.Callback<DataModel> {
+            override fun onResponse(call: Call<DataModel>, response: Response<DataModel>) {
+                if (response.isSuccessful) {
+                        listener.onmodeldata(response, "")
+                    }  else {
+                        Toast.makeText(context, "Failed to retrieve items", Toast.LENGTH_LONG).show()
+                    }
+                }
+            override fun onFailure(call: Call<DataModel>, t: Throwable) {
+                Toast.makeText(context, "Failed to retrieve details " + t.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+
+     fun UpdateSingleData(id: Int,listener: Fetchmodeldata) {
+            val city = "Thrissur"
+            val description = "your own data"
+            val country = "india"
+            val requestCall = destinationService.updateDestination(id, city, description, country)
+            requestCall.enqueue(object: Callback<DataModel> {
+                override fun onResponse(call: Call<DataModel>, response: Response<DataModel>) {
+                    if (response.isSuccessful) {
+                        listener.onmodeldata(response, "")
+                    } else {
+                        Toast.makeText(context, "Failed to update item", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onFailure(call: Call<DataModel>, t: Throwable) {
+                    Toast.makeText(context, "Failed to update item", Toast.LENGTH_SHORT).show()
+                }
+            })
+    }
+
+     fun DeleteSingleData(id: Int,listener: Fetchmodeldata) {
+
+            val requestCall = destinationService.deleteDestination(id)
+            requestCall.enqueue(object: Callback<Unit> {
+
+                override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(context, "Successfully Deleted", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context, "Failed to Delete", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<Unit>, t: Throwable) {
+                    Toast.makeText(context, "Failed to Delete", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
+    fun AddSingleData(city: String, des: String, country: String , listener: Fetchmodeldata){
+        val newDestination = DataModel()
+        newDestination.city = city
+        newDestination.description = des
+        newDestination.country = country
+        val requestCall = destinationService.addDestination(newDestination)
+        requestCall.enqueue(object: Callback<DataModel> {
+            override fun onResponse(call: Call<DataModel>, response: Response<DataModel>) {
+                if (response.isSuccessful) {
+                    listener.onmodeldata(response,"" )
+                    var newlyCreatedDestination = response.body() // Use it or ignore it
+                    Toast.makeText(context, "Successfully Added", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "Failed to add item", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onFailure(call: Call<DataModel>, t: Throwable) {
+                Toast.makeText(context, "Failed to add item", Toast.LENGTH_SHORT).show()
             }
         })
     }
