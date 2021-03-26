db0077d001ae56ad76f422e5613ae6016f2e945d
WhiteQube <vaisakhkmohan@gmail.com>
2021-03-26 00:15:16 -0700

    Update DataService.kt

--- app/src/main/java/com/vaisakh/hustler/retrofit_recyclerview_kotlin_mvvm/DataService.kt
+++ app/src/main/java/com/vaisakh/hustler/retrofit_recyclerview_kotlin_mvvm/DataService.kt
@@ -1,12 +1,34 @@
 package com.vaisakh.hustler.retrofit_recyclerview_kotlin_mvvm
 
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.QueryMap
+import retrofit2.http.*
 import java.util.logging.Filter
 
 interface DataService {
 
+    //get All data from destination
     @GET("destination")
     fun getDatalist(@QueryMap filter: HashMap<String, String>): Call<List<DataModel>>
+
+    //get data where id = "id"
+    @GET("destination/{id}")
+    fun getDestination(@Path("id") id: Int): Call<DataModel>
+
+    //post some data to url
+    @POST("destination")
+    fun addDestination(@Body newDestination: DataModel): Call<DataModel>
+
+    //update data where id = "id"
+    @FormUrlEncoded
+    @PUT("destination/{id}")
+    fun updateDestination(
+            @Path("id") id: Int,
+            @Field("city") city: String,
+            @Field("description") desc: String,
+            @Field("country") country: String
+    ): Call<DataModel>
+
+    //delete data where id = "id"
+    @DELETE("destination/{id}")
+    fun deleteDestination(@Path("id") id: Int): Call<Unit>
 }
